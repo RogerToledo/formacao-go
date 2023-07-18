98ea0074a4e9444f68dc79e3e6755c8ecde20a96
Roger Toledo <rogertoledo28@gmail.com>
2023-07-18 11:22:37 -0300

    feat: adiciona mux e api cliente

---
+++ api-cadastro/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"dio/formacao-go/manipulando-json/api-cadastro/handler"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	rota := mux.NewRouter()
+	rota.HandleFunc("/cliente", handler.ClienteHandler).Methods("POST")
+
+	fmt.Println("Escutando na porta : 8081")
+
+	log.Fatal(http.ListenAndServe(":8081", rota))
+}
